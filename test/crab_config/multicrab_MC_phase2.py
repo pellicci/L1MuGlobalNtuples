ceffabd6d6e8502efd6c86e1274bdb0917f54728
Mario <mpel@protonmail.com>
2019-02-04 17:42:35 +0100

    adding crab file

---
+++ test/crab_config/multicrab_MC_phase2.py
@@ -0,0 +1,47 @@
+from WMCore.Configuration import Configuration
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = Configuration()
+
+config.section_('General')
+config.General.transferOutputs = True
+config.General.workArea = 'crab_projects/samples_MC_phase2/'
+
+config.section_('JobType')
+config.JobType.psetName = 'run_L1MuNtuple.py'
+config.JobType.pluginName = 'Analysis'
+config.JobType.outputFiles = ['L1MuPhase2Ntuple_output.root']
+config.JobType.pyCfgParams = ['doPhase2Emul=True']
+
+config.section_('Data')
+config.Data.splitting = 'FileBased'
+config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
+config.Data.publication = False
+
+config.section_('Site')
+config.Site.storageSite = 'T2_IT_Legnaro'
+
+if __name__ == '__main__':
+
+    from CRABAPI.RawCommand import crabCommand
+    from CRABClient.ClientExceptions import ClientException
+    from httplib import HTTPException
+    from multiprocessing import Process
+
+    def submit(config):
+        try:
+            crabCommand('submit', config = config)
+        except HTTPException as hte:
+            print "Failed submitting task: %s" % (hte.headers)
+        except ClientException as cle:
+            print "Failed submitting task: %s" % (cle)
+
+    #############################################################################################
+    ## From now on that's what users should modify: this is the a-la-CRAB2 configuration part. ##
+    #############################################################################################
+    
+    config.General.requestName = 'WPiGammaAnalysis_ttbar'
+    config.Data.unitsPerJob = 5
+    config.Data.inputDataset = '/TT_TuneCUETP8M2T4_13TeV-powheg-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM'
+    p = Process(target=submit, args=(config,))
+    p.start()
+    p.join()
