d52883d2740bd69030c2fabf7341de111885f46c
Mario <mpel@protonmail.com>
2019-02-07 16:38:13 +0100

    adding crab configuration

--- test/crab_config/multicrab_MC_phase2.py
+++ test/crab_config/multicrab_MC_phase2.py
@@ -1,5 +1,4 @@
 from WMCore.Configuration import Configuration
-from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = Configuration()
 
 config.section_('General')
@@ -14,7 +13,7 @@ config.JobType.pyCfgParams = ['doPhase2Emul=True']
 
 config.section_('Data')
 config.Data.splitting = 'FileBased'
-config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
+config.Data.outLFNDirBase = '/store/user/pellicci/'
 config.Data.publication = False
 
 config.section_('Site')
@@ -39,9 +38,23 @@ if __name__ == '__main__':
     ## From now on that's what users should modify: this is the a-la-CRAB2 configuration part. ##
     #############################################################################################
     
-    config.General.requestName = 'WPiGammaAnalysis_ttbar'
-    config.Data.unitsPerJob = 5
-    config.Data.inputDataset = '/TT_TuneCUETP8M2T4_13TeV-powheg-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM'
+    config.General.requestName = 'L1MuPhase2Ntuples_SingleNu_PU200'
+    config.Data.unitsPerJob = 3
+    config.Data.inputDataset = '/SingleNeutrino/PhaseIIFall17D-L1TPU200_93X_upgrade2023_realistic_v5-v1/GEN-SIM-DIGI-RAW'
+    p = Process(target=submit, args=(config,))
+    p.start()
+    p.join()
+
+    config.General.requestName = 'L1MuPhase2Ntuples_SingleMuPt2to100_PU140'
+    config.Data.unitsPerJob = 3
+    config.Data.inputDataset = '/SingleMu_FlatPt-2to100/PhaseIIFall17D-L1TPU140_93X_upgrade2023_realistic_v5-v1/GEN-SIM-DIGI-RAW'
+    p = Process(target=submit, args=(config,))
+    p.start()
+    p.join()
+
+    config.General.requestName = 'L1MuPhase2Ntuples_SingleMuPt2to100_PU200'
+    config.Data.unitsPerJob = 3
+    config.Data.inputDataset = '/SingleMu_FlatPt-2to100/PhaseIIFall17D-L1TPU200_93X_upgrade2023_realistic_v5-v1/GEN-SIM-DIGI-RAW'
     p = Process(target=submit, args=(config,))
     p.start()
     p.join()
